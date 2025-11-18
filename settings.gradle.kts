d33da948183929150db7c3c92c363a0b49ffa1b4
Dipesh Pal <codewithdipesh2022@gmail.com>
2025-11-18 23:38:48 +0530

    initiated and added dependecies

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KanaSensei"
+include(":core")
+include(":core:data")
+include(":core:ui")
+include(":features")
+include(":features:auth")
+include(":features:learning")
+include(":features:kanaTable")
+include(":features:settings")
+include(":app")
