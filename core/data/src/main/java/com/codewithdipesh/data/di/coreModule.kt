b48fb510b21b4c46b8161460dd4bd37dc80aac6e
Dipesh Pal <codewithdipesh2022@gmail.com>
2025-12-13 12:40:16 +0530

    added di setup in core module

---
+++ core/data/src/main/java/com/codewithdipesh/data/di/coreModule.kt
@@ -0,0 +1,24 @@
+package com.codewithdipesh.data.di
+
+import com.codewithdipesh.data.connection.translation.TranslateRepository
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.koin.dsl.module
+
+val coreModule = module {
+
+    single {
+        HttpClient {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+    }
+
+    single { TranslateRepository(get()) }
+}
