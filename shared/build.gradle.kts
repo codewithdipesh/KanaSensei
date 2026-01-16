b33ad159ad3e239d4c4a28576a2f84dea439d749
Dipesh Pal <codewithdipesh2022@gmail.com>
2026-01-17 01:52:45 +0530

    added shared module and initated dependencies

---
+++ shared/build.gradle.kts
@@ -0,0 +1,121 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.android.lint)
+}
+
+kotlin {
+
+    androidLibrary {
+        namespace = "com.codewithdipesh.kanasensei.shared"
+        compileSdk = 36
+        minSdk = 24
+
+        withHostTestBuilder {
+        }
+
+        withDeviceTestBuilder {
+            sourceSetTreeName = "test"
+        }.configure {
+            instrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
+    }
+
+    val xcfName = "sharedKit"
+
+    iosX64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosSimulatorArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(libs.kotlin.stdlib)
+                // Add KMP dependencies here
+
+                // KMP Core dependencies
+                implementation(libs.bundles.kmp.common)
+
+                // Coroutines & Serialization for data models & business logic
+                implementation(libs.coroutines.core)
+                implementation(libs.serialization.json)
+                implementation(libs.kotlinx.datetime)
+
+                // Ktor for API calls
+                implementation(libs.bundles.kmp.ktor)
+
+                // Dependency Injection
+                implementation(libs.koin.core)
+
+                // Logging
+                implementation(libs.napier)
+            }
+        }
+
+        commonTest {
+            dependencies {
+                implementation(libs.kotlin.test)
+                implementation(libs.coroutines.test)
+            }
+        }
+
+        androidMain {
+            dependencies {
+                // Android KTX & Appcompat
+                implementation(libs.androidx.core.ktx)
+                implementation(libs.androidx.appcompat)
+                implementation(libs.material)
+
+                // Firebase
+                implementation(libs.firebase.firestore)
+                implementation(libs.firebase.auth)
+
+                // Android Ktor engine
+                implementation(libs.ktor.client.android)
+                implementation(libs.ktor.client.okhttp)
+
+                // Coroutines Android dispatcher
+                implementation(libs.coroutines.android)
+
+                // Koin for Android
+                implementation(libs.koin.android)
+
+                // Room (optional, if using local database)
+                implementation(libs.bundles.room)
+            }
+        }
+
+        getByName("androidDeviceTest") {
+            dependencies {
+                implementation(libs.androidx.runner)
+                implementation(libs.androidx.core)
+                implementation(libs.androidx.junit)
+            }
+        }
+
+        iosMain {
+            dependencies {
+                // Add iOS-specific dependencies here. This a source set created by Kotlin Gradle
+                // Plugin (KGP) that each specific iOS target (e.g., iosX64) depends on as
+                // part of KMP’s default source set hierarchy. Note that this source set depends
+                // on common by default and will correctly pull the iOS artifacts of any
+                // KMP dependencies declared in commonMain.
+            }
+        }
+    }
+
+}
