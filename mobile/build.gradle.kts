bf47ff0257f38b2ab3c8451df1f22ec475d4572b
Dipesh Pal <codewithdipesh2022@gmail.com>
2026-07-14 23:42:32 +0530

    version 1.0.1

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.codewithdipesh.kanasensei"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
